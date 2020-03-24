f11781ade1f79495e0aed42d60e94b13f953661a
Carlos Barrientos <carlosbarrientosguillen@gmail.com>
2020-03-24 19:55:13 +0100

    ejercicio palindromo a medias

--- js/entrega-2/www/palindromo.js
+++ js/entrega-2/www/palindromo.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let a = ["Arriba", "la", "birra"];
+
+let b = a.join("");
+let c = b.toLocaleLowerCase();
+
+let x = for (let i = 0; i < c.length; i++) {
+  console.log(c[i]);
+};
+
+let y = for (let i = c.length - 1; i >= 0; i--) {
+  console.log(c[i]);
+};
+
