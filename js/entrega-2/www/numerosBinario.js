da10ec3f6e9c7789a3358ed67587fa929fa55e18
Charlise9 <carlosbarrientosguillen@gmail.com>
2020-03-24 15:31:57 +0100

    comienzo numeros binarios

--- js/entrega-2/www/numerosBinario.js
+++ js/entrega-2/www/numerosBinario.js
@@ -1,3 +1,21 @@
 "use strict";
 
-let binaryNumber = 
+/* let binaryNumber = "157";
+
+for (let i = binaryNumber.length - 1; i >= 0; i--) {
+  console.log(binaryNumber[i]);
+}
+ */
+
+function binaryConverter(string) {
+  let decimal = +0;
+  let bits = +1;
+  for (let i = 0; i < string.length; i++) {
+    let currNum = +string[string.length - i - 1];
+    if (currNum === 1) {
+      decimal += bits;
+    }
+    bits *= 2;
+  }
+  console.log(decimal);
+}
