24dbee25a3fda5baa7c1adb2c8f325136594455a
Carlos Barrientos <carlosbarrientosguillen@gmail.com>
2020-04-26 20:16:45 +0200

    entrega 4 completa

---
+++ js/entrega-3/www/e-commerce.js
@@ -0,0 +1,93 @@
+"use strict";
+
+/*Crear un falso e-commerce. Por un lado tenemos todos los artículos que forman
+ el stock de la tienda. Tendremos también un usuario que añade cosas a su carrito,
+ que es privado. Los artículos cuando los metemos al carrito los convertimos un tipo de
+ dato que guarda las unidades del usuario de dicho artículo. La tienda es la
+ encargada de imprimir un ticket por la consola.*/
+
+const itemNames = ["Camisa", "Pantalon", "Calcetines"];
+const itemPrices = [13, 27, 100];
+//const items = Item.buildItems(itemNames, itemPrices);
+//const myUser = new User();
+
+class Item {
+  itemType;
+  price;
+
+  constructor(itemType, price) {
+    this.itemType = itemType;
+    this.price = price;
+  }
+}
+
+// elaboramos un array con todos los items a la venta y sus precios:
+
+const inventary = itemNames.map((name, index) => {
+  return new Item(name, itemPrices[index]);
+});
+
+class User {
+  #carrito = [];
+
+  addItems(item) {
+    let foundItem = this.#carrito.find((element) => {
+      //console.log(element, item);
+      return element.itemData.itemType === item.itemType;
+    });
+    if (foundItem) {
+      foundItem.ammount++;
+    } else {
+      this.#carrito.push({ itemData: item, ammount: 1 });
+    }
+
+    // Primero busco si está en el carrito
+
+    // Si no habia, lo meto
+    //else {s
+    //Si existia le incremento las unidades
+    //
+  }
+
+  leerCarrito() {
+    return this.#carrito;
+  }
+}
+
+const myUser = new User();
+myUser.addItems(inventary[0]);
+myUser.addItems(inventary[0]);
+myUser.addItems(inventary[0]);
+myUser.addItems(inventary[1]);
+myUser.addItems(inventary[2]);
+myUser.addItems(inventary[2]);
+myUser.addItems(inventary[2]);
+myUser.addItems(inventary[2]);
+myUser.addItems(inventary[2]);
+//console.log(myUser);
+
+class Shop {
+  carrito;
+
+  constructor(carrito) {
+    this.carrito = carrito.leerCarrito();
+  }
+  getTicket() {
+    this.carrito.map((element) => {
+      return (element.precioPorElemento =
+        element.ammount * element.itemData.price);
+    });
+    return this.carrito;
+  }
+  getTotal() {
+    const total = this.carrito.reduce((precio, cadaItem) => {
+      return precio + cadaItem.precioPorElemento;
+    }, 0);
+    return console.log(`La factura asciende a ${total} €`);
+  }
+}
+
+const ticket = new Shop(myUser);
+//console.log(ticket);
+console.log(ticket.getTicket());
+ticket.getTotal();
