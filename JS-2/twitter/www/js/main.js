2ccc5807a29a36c6473c80ed2eafef317b6473b0
Carlos Barrientos <carlosbarrientosguillen@gmail.com>
2020-06-03 13:56:03 +0200

    twitter hecho, no optimizado

--- JS-2/twitter/www/js/main.js
+++ JS-2/twitter/www/js/main.js
@@ -1,20 +1,56 @@
 "use strict";
 
-// SELECTORES
-const button = document.querySelector("button.enviar");
-const tweet = document.querySelector("textarea#tweet");
-const wall = document.querySelector(".wall");
+const form = document.querySelector("form");
+const list = document.querySelector("ul#tweets");
 
-// CREADORES
-const ul = document.createElement("ul");
+const addTweet = (text) => {
+  const li = document.createElement("li");
 
-const li = document.createElement("li");
+  const p = document.createElement("p");
+  p.textContent = text;
 
-wall.appendChild(ul);
+  const footer = document.createElement("footer");
 
-const handleClick = function () {
-  li.innerHTML = "hola";
-  ul.appendChild(li);
+  const time = document.createElement("time");
+  const now = new Date();
+  time.textContent = `${now.getDate()}/${now.getMonth()}/${now.getFullYear()}`;
+
+  const button = document.createElement("button");
+  button.classList.add("action");
+  button.textContent = "Borrar";
+
+  footer.append(time);
+  footer.append(button);
+
+  li.append(p);
+  li.append(footer);
+
+  list.prepend(li);
 };
 
-button.addEventListener("click", handleClick);
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  const input = form.querySelector("input#tweet");
+
+  const text = input.value;
+
+  if (text.length === 0 || text.length > 250) {
+    alert("Sosio, escribe algo que no se pase de 250 caracteres");
+  } else {
+    addTweet(text);
+    input.value = "";
+  }
+};
+
+const handleListClick = (event) => {
+  const target = event.target;
+
+  if (target.matches("ul button.action")) {
+    target.closest("li").remove();
+  }
+};
+
+form.addEventListener("submit", handleFormSubmit);
+
+document.body.addEventListener("click", handleListClick);
