8e6aa066b36d9c1b4333f47c1e00b27ec52a6e99
Carlos Barrientos <carlosbarrientosguillen@gmail.com>
2020-06-03 12:34:20 +0200

    app ToDo List completa, publicación en internet incluida

--- JS-2/app-tareas/www/js/main.js
+++ JS-2/app-tareas/www/js/main.js
@@ -2,17 +2,78 @@
 
 class TodoList {
   constructor(form, list, keyName) {
+    // Nombre del key con el que guardar la lista en el local Storage
     this.keyName = keyName;
 
+    // Lista de todos
     this.todos = [];
 
+    // Miro a ver si hay guardados en el localStorage y si los hay los uso como todos iniciales
     const storedTodos = JSON.parse(window.localStorage.getItem(this.keyName));
 
     if (storedTodos) {
       this.todos = storedTodos;
     }
+
+    this.form = form;
+    this.list = list;
+    this.cleanButton = this.form.querySelector("button.clean");
+    this.deleteAllButton = this.form.querySelector("button.delete");
+
+    if (
+      !this.form ||
+      !this.list ||
+      !this.cleanButton ||
+      !this.deleteAllButton
+    ) {
+      throw new Error("Hay elementos que faltan, comprueba tu HTML");
+    }
+
+    this.render();
   }
 
+  attachEvents() {
+    // Cuando se envía el formulario ejecute el método addTodo con el texto del input
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const input = this.form.querySelector("input#todo");
+
+      if (input.value.length > 0 && input.value.length < 200) {
+        this.addTodo(input.value);
+
+        input.value = "";
+      }
+      console.log(this.todos);
+    });
+
+    // Cuando se pulse el botón de limpiar tareas ejecute el método cleanTodoList
+    this.cleanButton.addEventListener("click", (e) => {
+      this.cleanTodoList();
+    });
+
+    // Cuando se pulse el botón de borrar todo ejecute el método deleteAllTodos
+    this.deleteAllButton.addEventListener("click", (e) => {
+      if (
+        window.prompt("Escribe BORRAR para borrar todos los todos") === "BORRAR"
+      ) {
+        this.deleteAllTodos();
+      }
+    });
+
+    // Evento delegado: cada vez que clico en la lsita, di donde clico es en el checkbox,
+    //marco el todo como hecho
+
+    this.list.addEventListener("click", (e) => {
+      const target = event.target;
+
+      if (target.matches('input[type= "checkbox"]')) {
+        this.toggleTodoStatus(target.getAttribute("data-index"));
+      }
+    });
+  }
+
+  // ESte método añade un todo
   addTodo(text) {
     if (text.length === 0) throw new Error("El texto del todo está vacío");
 
@@ -26,12 +87,14 @@ class TodoList {
     this.sync();
   }
 
+  // Este método borra todos los todos
   deleteAllTodos() {
     this.todos = [];
 
     this.sync();
   }
 
+  // Este método cambia el estado del todo (index) a hecho o a pendiente
   toggleTodoStatus(index) {
     const todo = this.todos[index];
 
@@ -40,6 +103,7 @@ class TodoList {
     this.sync();
   }
 
+  // Este método limpia los todos hechos
   cleanTodoList() {
     const cleanList = this.todos.filter((todo) => !todo.done);
 
@@ -48,14 +112,51 @@ class TodoList {
     this.sync();
   }
 
+  // Este método sincroniza la lista actual de todos con el localStorage
+  // y escribe los todos en el elemento html
   sync() {
     window.localStorage.setItem(this.keyName, JSON.stringify(this.todos));
+
+    this.render();
+  }
+
+  // Escribe la lista de todos en el elemento html
+  render() {
+    this.list.innerHTML = "";
+
+    const fragment = document.createDocumentFragment();
+
+    let i = 0;
+
+    for (const todo of this.todos) {
+      const li = document.createElement("li");
+      const input = document.createElement("input");
+      input.setAttribute("type", "checkbox");
+      input.setAttribute("data-index", i);
+
+      if (todo.done) {
+        li.classList.add("done");
+        input.setAttribute("checked", true);
+      }
+
+      const p = document.createElement("p");
+      p.textContent = todo.text;
+
+      li.append(input);
+      li.append(p);
+
+      fragment.append(li);
+
+      i++;
+    }
+    this.list.append(fragment);
   }
 }
 
 // Selecciono los elementos
-const form = document.querySelector("form#add-todo");
+const form = document.querySelector("form#new-todo");
 const list = document.querySelector("ul#todos");
 
 //Instancia de la clase!
 const myTodos = new TodoList(form, list, "stored-todos");
+myTodos.attachEvents();
